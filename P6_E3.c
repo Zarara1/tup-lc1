30220ed1c33ba882bfc7eb8f6f4c312c888599fc
Ibes <ibesmellano4@gmail.com>
2023-06-23 19:14:29 -0300

    Ejercicio 3

--- P6_E3.c
+++ P6_E3.c
@@ -2,21 +2,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char primera_letra(char letra); 
+
+char primera_letra(char* letra)
+{
+return letra[0];
+}
 
 int main ()
 {
-char letra,primera_letra,letra1;
+char palabra[20];
+printf("Ingrese una palabra\n");
+scanf ("%s",&palabra);
+
+printf("La primer letra de la palabra es: %c",primera_letra(palabra));
 
 
 
-
-
-}
-
-
-char primera_letra(char letra)
-{
-return letra[0];
 }
